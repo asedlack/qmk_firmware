5a435293a121173af0a77f95eba16fd4d6e2aa7c
asedlack <asedlack96@gmail.com>
2025-03-09 14:59:16 -0400

    Adds process_record_unicode for unicode macro sequences.

--- keyboards/asedlack/macrostick/process_records.c
+++ keyboards/asedlack/macrostick/process_records.c
@@ -1,6 +1,11 @@
 enum custom_keycodes {
   KVM_1 = SAFE_RANGE,
   KVM_2,
+  UC_FLIP,         // (ಠ痊ಠ)┻━┻
+  UC_SHRG,         // ¯\_(ツ)_/¯
+  UC_DISA,         // ಠ_ಠ
+  UC_IRNY,         // ⸮
+  UC_CLUE,         // ‽
 };
 
 // Process KVM inputs
@@ -22,5 +27,40 @@ bool process_record_kvm(uint16_t keycode, keyrecord_t *record) {
       }
       break;
   }
+  return true;
+};
+
+// Process unicode macros
+bool process_record_unicode(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case UC_FLIP: // (ノಠ痊ಠ)ノ彡┻━┻
+      if (record->event.pressed) {
+          send_unicode_string("(ノಠ痊ಠ)ノ彡┻━┻");
+      }
+      break;
+
+    case UC_SHRG: // ¯\_(ツ)_/¯
+      if (record->event.pressed) {
+          send_unicode_string("¯\\_(ツ)_/¯");
+      }
+      break;
+
+    case UC_DISA: // ಠ_ಠ
+      if (record->event.pressed) {
+          send_unicode_string("ಠ_ಠ");
+      }
+      break;
+
+    case UC_IRNY: // ⸮
+      if (record->event.pressed) {
+          register_unicode(0x2E2E);
+      }
+      break;
+    case UC_CLUE: // ‽
+      if (record->event.pressed) {
+          register_unicode(0x203D);
+      }
+      break;
+    }
   return true;
 };
